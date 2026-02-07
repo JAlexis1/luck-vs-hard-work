b46fc49b5b6c943c01d304164de35464cc6242fa
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2026-02-07 16:01:10 -0500

    feat: add method for getting top n candidates

--- src/simulation.py
+++ src/simulation.py
@@ -19,3 +19,14 @@ class Simulation:
             sorted_with_luck.append(sorted(score_with_luck.items(), key=lambda x: x[1][0], reverse=True))
             sorted_without_luck.append(sorted(score_without_luck.items(), key=lambda x: x[1], reverse=True))
         return sorted_with_luck, sorted_without_luck
+    
+    def get_top_n_candidates(self, sorted_candidates, top_n):
+        if top_n > len(sorted_candidates[0]):
+            raise ValueError("top_n must be less than or equal to the number of candidates")
+        top_candidates = []
+        for i in range(len(sorted_candidates)):
+            selected_candidates = [] # auxiliar list to store the top n candidates for each run
+            for j in range(top_n):
+                selected_candidates.append(sorted_candidates[i][j][0])
+            top_candidates.append(selected_candidates)
+        return top_candidates
